7e5900d698c4f40d7f148ae63dee71e401607571
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-03-13 14:58:18 -0700

    using decrementation

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * using only putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
