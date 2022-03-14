b770eefd223a5ab8ab66566f9b6a66e87bc574a7
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-03-14 07:11:28 -0700

    combination of 2 different digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combination of two digits
+ * use only putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 8; n++)
+	{
+		for (m = 1; m < 9; m++)
+		{
+			if (m > n)
+			{
+				putchar((n) + '0');
+				putchar((m) + '0');
+				if (n != 8 && m != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
