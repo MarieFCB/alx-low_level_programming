2f1c78267e72ba2d48339f4211bbb8147fbe7dc3
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-03-16 16:02:55 -0700

    every minute of of jack bauer day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,11 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(n + '0');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 			_putchar(':');
-			_putchar(m + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
