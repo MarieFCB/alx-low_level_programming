2a1e4c39dc2ad23a1bc4266d2b2b38fcc057b00a
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-03-16 14:33:27 -0700

    integer absolute

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 
 /**
  * print_sign - prints the sign of number
- * @n: is the number it checks the sign
+ * @n: is the number to check the sign
  * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
