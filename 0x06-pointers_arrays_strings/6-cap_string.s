f1f9c858649ae14a44eadebd30ad6bff18afbbc4
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-03-25 17:10:48 -0700

    capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.s
@@ -0,0 +1,141 @@
+	.file	"6-cap_string.c"
+	.text
+	.globl	cap_string
+	.type	cap_string, @function
+cap_string:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -40(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movabsq	$4548967930297714976, %rax
+	movq	%rax, -21(%rbp)
+	movl	$2066294818, -13(%rbp)
+	movb	$125, -9(%rbp)
+	movl	$0, -32(%rbp)
+	jmp	.L2
+.L7:
+	cmpl	$0, -32(%rbp)
+	jne	.L3
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$96, %al
+	jle	.L3
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$122, %al
+	jg	.L3
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	leal	-32(%rax), %ecx
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movl	%ecx, %edx
+	movb	%dl, (%rax)
+.L3:
+	movl	$0, -28(%rbp)
+	jmp	.L4
+.L6:
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %edx
+	movl	-28(%rbp), %eax
+	cltq
+	movzbl	-21(%rbp,%rax), %eax
+	cmpb	%al, %dl
+	jne	.L5
+	movl	-32(%rbp), %eax
+	cltq
+	leaq	1(%rax), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$96, %al
+	jle	.L5
+	movl	-32(%rbp), %eax
+	cltq
+	leaq	1(%rax), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$122, %al
+	jg	.L5
+	movl	-32(%rbp), %eax
+	cltq
+	leaq	1(%rax), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	leal	-32(%rax), %ecx
+	movl	-32(%rbp), %eax
+	cltq
+	leaq	1(%rax), %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movl	%ecx, %edx
+	movb	%dl, (%rax)
+.L5:
+	addl	$1, -28(%rbp)
+.L4:
+	cmpl	$12, -28(%rbp)
+	jle	.L6
+	addl	$1, -32(%rbp)
+.L2:
+	movl	-32(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-40(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L7
+	movq	-40(%rbp), %rax
+	movq	-8(%rbp), %rsi
+	xorq	%fs:40, %rsi
+	je	.L9
+	call	__stack_chk_fail@PLT
+.L9:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	cap_string, .-cap_string
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
