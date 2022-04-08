7fbddf9c28d3572539cb512742bb9eef8934616b
MarieFCB <bleoumariefrancecoco@gmail.com>
2022-04-08 06:00:17 -0700

    frees a 2 dimensional grid

---
+++ 0x0B-malloc_free/4-free_grid.s
@@ -0,0 +1,67 @@
+	.file	"4-free_grid.c"
+	.text
+	.globl	free_grid
+	.type	free_grid, @function
+free_grid:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	cmpq	$0, -24(%rbp)
+	je	.L7
+	cmpl	$0, -28(%rbp)
+	je	.L7
+	movl	$0, -4(%rbp)
+	jmp	.L5
+.L6:
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	addl	$1, -4(%rbp)
+.L5:
+	movl	-4(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L6
+	movq	-24(%rbp), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	jmp	.L1
+.L7:
+	nop
+.L1:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	free_grid, .-free_grid
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
